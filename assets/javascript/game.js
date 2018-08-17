7493280a6577f3a8892c9de0bc9afe82d4eb3a18
Radsauce <radley.eakle@yahoo.com>
2018-08-16 18:25:43 -0700

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,45 @@
 let score = 0
 let currentWord = ""
-let currentGuesses = []
-let wrongLetters = []
+let currentGuesses = ['']
+let wrongLetters = ['']
 let guessesLeft = 10
 
 const wordList = ['WOLVERINE', 'CYCLOPS', 'JEAN GREY', 'PSYLOCKE', 'ANGEL', 'ARCHANGEL', 'STORM', 'BEAST', 'GAMBIT', 'NIGHTCRAWLER', 'DAZZLER', 'COLOSSUS', 'SHADOWCAT', 'ICEMAN', 'JUBILEE', 'PROFESSOR X', 'ROGUE', 'MORPH']
 
 function restartRound() {
-  currentWord = randWord(wordList)
+  currentWord = randWord()
   guessesLeft = 10;
   wrongLetters = [];
 }
 
 function displayValues() {
   let scoreTotal = document.querySelector("#score")
-  let hiddenWord = document.querySelector("#hiddenWord")
+  let currentWord = document.querySelector("#currentWord")
   let wrongLetters = document.querySelector("#wrongLetters")
   let guessesLeft = document.querySelector("#guessesLeft")
 
-  scoreTotal.innerHTML = 0
-  hiddenWord.innerHTML = hiddenWord()
-  wrongLetters.innerHTML = wrongLetters.join()
-  guessesLeft.innerHTML = guessesLeft
+   scoreTotal.innerHTML = 0
+   currentWord.innerHTML = hiddenWord()
+   wrongLetters.innerHTML = wrongLetters
+   guessesLeft.innerHTML = guessesLeft
 }
 
+document.querySelector("#score").innerHTML = displayValues()
+document.querySelector("#currentWord").innerHTML = displayValues()
+document.querySelector("#wrongLetters").innerHTML = displayValues()
+document.querySelector("#guessesLeft").innerHTML = displayValues()
 
-function randWord(arr) {
-  return arr[Math.floor(Math.rand) * wordList.length]
+
+function randWord() {
+  let arr = [Math.floor(Math.random() * wordList.length)]
+  return arr
 }
 
 document.addEventListener("keypress", function(event) {
   let letter = event.key.toUpperCase();
   currentGuesses.push(letter);
-    if(guessesLeft <= 0)
-  {
-  restartRound();
+    if(guessesLeft <= 0) {
+      restartRound();
     } 
     if(hiddenWord() === currentWord) {
       score++
@@ -46,6 +51,7 @@ document.addEventListener("keypress", function(event) {
       guessesLeft --
       wrongLetters.push(letter)
     }
+    console.log(letter)
     displayValues()
     })
 
